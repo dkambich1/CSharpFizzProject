ab9f06b1e0c577fe07efc465a6980f844e903eba
dkambich <david.kambich@keysight.com>
2025-02-21 16:05:25 -0700

    Commit 2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Globalization;
+
+
+
+
+class Program
+{
+    static void Main()
+    {
+
+        Console.WriteLine("New Features for Commit 2");
+
+    }
+}
